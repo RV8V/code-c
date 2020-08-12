a0a43606a7f0dc486af9f1249555c948b6b21496
Ruslan <rv.volovik88@gmail.com>
2020-08-12 12:59:28 +0300

    quick sort of numbers and strings

--- qs.random.c
+++ qs.random.c
@@ -7,9 +7,9 @@ int sort_numbers(const void *a, const void *b);
 int sort_strings(const void *a, const void *b);
 
 int main(void) {
-  const short nums_size = 51;
-  const short buf_size = 6;
-  const short values_count = 7;
+  const unsigned short nums_size = 51;
+  const unsigned short buf_size = 6;
+  const unsigned short values_count = 7;
   short nums[nums_size];
   char buf[buf_size];
   char str[nums_size * 2] = "your 6 numbers: ";
@@ -17,10 +17,10 @@ int main(void) {
   char *strings[] = { "o", "s", "d" };
 
   srand(time(NULL));
-  for (short i; i < sizeof(nums - 1) / sizeof(int); i += 1)
+  for (unsigned short i; i < sizeof(nums - 1) / sizeof(int); i += 1)
     nums[i] = i;
 
-  for (short i; i < sizeof(nums - 1) / sizeof(int); i += 1) {
+  for (unsigned short i; i < sizeof(nums - 1) / sizeof(int); i += 1) {
     r = (rand() % sizeof(nums)) + 1;
     temp = nums[i];
     nums[i] = nums[r];
@@ -28,14 +28,14 @@ int main(void) {
   }
 
   qsort(nums, nums_size, sizeof(int), sort_numbers);
-  for (short i; i < nums_size; i += 1)
+  for (unsigned short i; i < nums_size; i += 1)
     fprintf(stdout, "%i", nums[i]);
 
   qsort(strings, sizeof(strings), sizeof(char), sort_strings);
-  for (short i; i < sizeof(strings); i += 1)
+  for (unsigned short i; i < sizeof(strings); i += 1)
     fprintf(stdout, "%s\n", strings[i]);
 
-  for (short i; i < values_count; i += 1) {
+  for (unsigned short i; i < values_count; i += 1) {
     sprintf(buf, "%d", nums[i]);
     strcat(buf, " ");
     strcat(str, buf);
