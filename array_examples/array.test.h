ff61d4ebdbc4d8a7acbb00ec1a177eb0ab8d8529
Ruslan <rv.volovik88@gmail.com>
2020-08-15 14:05:49 +0300

    also division is needed, test example for arrays (farm and player)

--- array_examples/array.test.h
+++ array_examples/array.test.h
@@ -3,6 +3,7 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define PARAMS_COUNT 4
 #define POWER 0
@@ -10,7 +11,42 @@
 #define INTELLEGENCE 2
 #define POWER_2 3
 
+#define COUNT_FOOD_FARM 5
+#define COUNT_OIL_FARM 5
+#define FARM_K 1.2
+
+unsigned short food_farm[COUNT_FOOD_FARM] = { 1 };
+unsigned short oil_farm[COUNT_OIL_FARM] = { 1 };
+
+unsigned short oil_warehouse = 0;
+unsigned short food_warehouse = 0;
+
 unsigned char player_params[PARAMS_COUNT] = { 0 };
+
+void resource_processing(void) {
+  float food_produce = 0.0f, oil_produce = 0.0f;
+
+  char i = 0;
+  for (; i < COUNT_FOOD_FARM; ++i)
+    if ((rand() % 30) == 5) food_farm[i]++;
+
+  i = 0;
+  for (; i < COUNT_OIL_FARM; ++i)
+    if ((rand() % 30) == 5) oil_farm[i]++;
+
+  for (i = 0; i < COUNT_FOOD_FARM; ++i)
+    food_produce += food_farm[i] * FARM_K;
+  for (i = 0; i < COUNT_OIL_FARM; ++i)
+    oil_produce += oil_farm[i] * FARM_K;
+  food_warehouse += food_produce;
+  oil_warehouse += oil_produce;
+
+  system("clear");
+  fprintf(stdout, "food produce (h) -> %0.f\n", food_produce * 3600.f);
+  fprintf(stdout, "oil produce (h) -> %0.f\n", oil_produce * 3600.f);
+  fprintf(stdout, "food warehouse: %d\n", food_warehouse);
+  fprintf(stdout, "oil warehouse: %d\n", oil_warehouse);
+}
 
 void print_common_power(void) {
   char i = PARAMS_COUNT, result = 0;
@@ -36,8 +72,11 @@ void init_param_values(void) {
 }
 
 void main_test2(void) {
-  init_param_values();
-  print_params();
+  do {
+    resource_processing();
+    sleep(1);
+  }
+  while(1);
 }
 
 #endif
