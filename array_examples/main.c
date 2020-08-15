369d52ce54cbded95c01c5a86375c27857cd2fbe
Ruslan <rv.volovik88@gmail.com>
2020-08-15 12:57:22 +0300

    second test code snippet for arrays

--- array_examples/main.c
+++ array_examples/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-#include "array.h"
+#include "example.test.h"
+#include "test.h"
+
 
 int main(int argc, char *argv[]) {
-  main_test1();
+  //main_test1();
+  main_test2();
   return 0;
 }
