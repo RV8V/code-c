b175573448193f707099de6fda4e97f5b188545b
Ruslan <rv.volovik88@gmail.com>
2020-08-15 17:05:28 +0300

    initial commit for sea battle game

---
+++ array_examples/battle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+/*
+     ABCDEFGHIJ       ABCDEFGHIJ
+    *----------*     *----------*
+  0 |          |   0 |          |
+  1 |          |   1 |          |
+  2 |          |   2 |          |
+  3 |          |   3 |          |
+  4 |          |   4 |          |
+  5 |          |   5 |          |
+  6 |          |   6 |          |
+  7 |          |   7 |          |
+  8 |          |   8 |          |
+  9 |          |   9 |          |
+    *----------*     *----------*
+*/
+
+int main() {
+
+  return 0;
+}
