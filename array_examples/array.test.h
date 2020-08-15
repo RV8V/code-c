dca575774402e3bdca12a526246047431fdc81ca
Ruslan <rv.volovik88@gmail.com>
2020-08-15 13:24:29 +0300

    also division is needed, test example for arrays

--- array_examples/array.test.h
+++ array_examples/array.test.h
@@ -1,20 +1,43 @@
 #ifndef TEST_H
 #define TEST_H
 
-unsigned char player_params[2] = { 0 };
+#include <math.h>
+#include <stdlib.h>
+
+#define PARAMS_COUNT 4
+#define POWER 0
+#define ARMOR 1
+#define INTELLEGENCE 2
+#define POWER_2 3
+
+unsigned char player_params[PARAMS_COUNT] = { 0 };
 
 void print_common_power(void) {
-  char i = 2, result = 0;
+  char i = PARAMS_COUNT, result = 0;
   do result += player_params[i];
   while(i-- > 0);
   printf("common: %d\n", result);
 }
 
-void main_test2() {
-  player_params[0] = 1;
-  player_params[1] = 2;
+void print_params(void) {
+  unsigned char i = 0;
+  for (; i < PARAMS_COUNT; ++i) {
+    if (i == POWER) fprintf(stdout, "power: %i\n", player_params[POWER]);
+    if (i == ARMOR) fprintf(stdout, "armor: %i\n", player_params[ARMOR]);
+    if (i == POWER_2) fprintf(stdout, "power_2: %i\n", player_params[POWER_2]);
+    if (i == INTELLEGENCE) fprintf(stdout, "intellegence: %i\n", player_params[INTELLEGENCE]);
+  }
+}
 
-  print_common_power();
+void init_param_values(void) {
+  char i = PARAMS_COUNT;
+  do player_params[i] = rand() % 40;
+  while(i-- > 0);
+}
+
+void main_test2(void) {
+  init_param_values();
+  print_params();
 }
 
 #endif
