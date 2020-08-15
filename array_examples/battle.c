19fd6ed78f141380f6d88c88bcac95fe92f3b61b
Ruslan <rv.volovik88@gmail.com>
2020-08-15 17:56:45 +0300

    initial commit for sea battle game

--- array_examples/battle.c
+++ array_examples/battle.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 #define N_LINES 13
+#define FIELD_SIZE 10
+
+typedef enum { EMPTY = 0, SHOT, STRIKE, KILL, SHIP, FIELD_INFO_END } fiedInfo;
+
+char draw_symbol[FIELD_INFO_END] = { ' ', '*', 'x', '#', '&' };
 
 char *field[] = {
   "   ABCDEFGHIJ       ABCDEFGHIJ ",
@@ -19,8 +24,27 @@ char *field[] = {
   "  *----------*     *----------*"
 };
 
+void draw_field(void);
+
 int main() {
-  for (int i = 0; i < N_LINES; i++)
-    printf("%s\n", field[i]);
+  draw_field();
   return 0;
+}
+
+void draw_field(void) {
+  int i = 0;
+  do fprintf(stdout, "%s\n", field[i]);
+  while(i++ < 1);
+
+  for (int i = 0; i < 10; i++) {
+    fprintf(stdout, "%c%c", field[i + 2][0], field[i + 2][2]);
+    for (int j = 0; j < FIELD_SIZE; j++)
+      fprintf(stdout, " ");
+    for (int j = 12; j < 20; j++)
+      fprintf(stdout, "%c", field[i + 2][j]);
+    for (int j = 0; j < FIELD_SIZE; j++)
+      fprintf(stdout, " ");
+    fprintf(stdout, "%c\n", field[i + 2][30]);
+  }
+  fprintf(stdout, "%s\n", field[N_LINES - 1]);
 }
