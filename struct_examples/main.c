51a930e12bfd93a2ccac7c02189be57a26a22e51
Ruslan <rv.volovik88@gmail.com>
2020-08-16 14:20:51 +0300

    entry point for testing structs and alignment

--- struct_examples/main.c
+++ struct_examples/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "struct.h"
 
+#pragma pack(push, 1)
+typedef struct { char ch; int i; } Data;
+#pragma pack(pop)
+
 int main(int argc, char const *argv[]) {
+  Data data;
+  data.ch = 0x11;
+  data.i = 0x02030405;
+  char *p = (char *)&data;
+  p++;
+  *p = 90; // 5a
   s_a_print_info();
   return 0;
 }
