a9846788fb4e25aae3e4bd96cdd03d2e3db6d9ed
Ruslan <rv.volovik88@gmail.com>
2020-08-16 21:03:45 +0300

    initial commit for pointers for functions

--- pointer_to_function/main.c
+++ pointer_to_function/main.c
@@ -5,9 +5,23 @@ void foo_a() {
   printf("%s\n", __FUNCTION__);
 }
 
+void foo_b() {
+  printf("%s\n", __FUNCTION__);
+}
+
+void foo_c() {
+  printf("%s\n", __FUNCTION__);
+}
+
 int main(int argc, char const *argv[]) {
-  void (*pf)() = &foo_a;
-  pf();
+  void (*pf)();
+  pf = &foo_a;
+  (*pf)();
+
+  pf = &foo_b;
+  (*pf)();
+
+  pf = &foo_c;
   (*pf)();
   return 0;
 }
