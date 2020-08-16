ef3b5d8a3bf761bc0a77c470b2c87f66366fa0ca
Ruslan <rv.volovik88@gmail.com>
2020-08-16 21:23:16 +0300

    initial commit for pointers for functions

--- pointer_to_function/fn.test.c
+++ pointer_to_function/fn.test.c
@@ -13,7 +13,21 @@ void foo_c() {
   printf("%s\n", __FUNCTION__);
 }
 
+typedef int *pint;
+typedef void (*pfn_v)();
+
 int main(int argc, char const *argv[]) {
+  pint pa, pb, pc;
+  printf("%p-%p-%p\n", &pa, &pb, &pc);
+
+  pfn_v a, b, c;
+  a = &foo_a, b = &foo_b, c = &foo_c;
+
+  (*a)(), (*b)(), (*c)();
+
+  void (*ps)() = &foo_a;
+  ps();
+
   void (*pf)();
   pf = &foo_a;
   (*pf)();
