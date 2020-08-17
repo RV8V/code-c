ec0e2179233a91e45e78adaa5ec29fe904128f6b
Ruslan <rv.volovik88@gmail.com>
2020-08-17 13:34:57 +0300

    bubble sort example for sorting arrays of integers, structures

--- pointer_to_function/main.c
+++ pointer_to_function/main.c
@@ -5,10 +5,15 @@
 
 #define ARRAY_SIZE 30
 
+typedef enum { E_SO_ASCENDING = 0, E_SO_DESCENDING, E_SO_CUSTOM, E_SO_END } SortOrder;
+
 bool pred_ascending(unsigned, unsigned);
 bool pred_descending(unsigned, unsigned);
+bool pred_custom(unsigned, unsigned);
 
-typedef enum { E_SO_ASCENDING = 0, E_SO_DESCENDING, E_SO_END } SortOrder;
+bool pred_ascending_s(struct Test, struct Test);
+bool pred_descending(struct Test, struct Test);
+bool pred_custom(struct Test, struct Test);
 
 int main(int argc, char const *argv[]) {
   int iarray[ARRAY_SIZE] = { 0 };
@@ -27,6 +32,7 @@ int main(int argc, char const *argv[]) {
   pf_compare pfa[E_SO_END];
   pfa[E_SO_ASCENDING] = &pred_ascending;
   pfa[E_SO_DESCENDING] = &pred_descending;
+  pfa[E_SO_CUSTOM] = &pred_custom;
 
   for (char i = 0; i < E_SO_END, i++) {
     ia_bubble_sort(&iarray[0], ARRAY_SIZE, pfa[i]);
@@ -37,10 +43,20 @@ int main(int argc, char const *argv[]) {
   return 0;
 }
 
+void ia_print_s(struct Test *apArray, unsigned aSize) {
+  for (unsigned i = 0; i < aSize; i++)
+    printf("%d", apArray[i].data);
+  printf("%\n");
+}
+
 bool pred_ascending(unsigned int a, unsigned int b) {
   return a >= b;
 }
 
 bool pred_descending(unsigned int a, unsigned int b) {
   return a < b;
+}
+
+bool pred_custom(unsigned int a, unsigned int b) {
+  return a + b > 400;
 }
