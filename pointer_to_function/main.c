cd800baca38296fb702c3f028632158a3b2af8ed
Ruslan <rv.volovik88@gmail.com>
2020-08-17 13:17:39 +0300

    initial commit for pointers for functions

--- pointer_to_function/main.c
+++ pointer_to_function/main.c
@@ -5,12 +5,42 @@
 
 #define ARRAY_SIZE 30
 
+bool pred_ascending(unsigned, unsigned);
+bool pred_descending(unsigned, unsigned);
+
+typedef enum { E_SO_ASCENDING = 0, E_SO_DESCENDING, E_SO_END } SortOrder;
+
 int main(int argc, char const *argv[]) {
   int iarray[ARRAY_SIZE] = { 0 };
   for (int i = 0; i < ARRAY_SIZE; ++i)
     iarray[i] = rand() % 1000;
-  ia_print(iarray, ARRAY_SIZE);
-  ia_bubble_sort(iarray, ARRAY_SIZE);
-  ia_print(iarray, ARRAY_SIZE);
+
+  ia_print(&iarray[0], ARRAY_SIZE);
+  ia_bubble_sort(&iarray[0], ARRAY_SIZE, &pred_ascending);
+  printf("\n\n");
+  ia_print(&iarray[0], ARRAY_SIZE);
+
+  ia_bubble_sort(&iarray[0], ARRAY_SIZE, &pred_descending);
+  printf("\n\n");
+  ia_print(&iarray[0], ARRAY_SIZE);
+
+  pf_compare pfa[E_SO_END];
+  pfa[E_SO_ASCENDING] = &pred_ascending;
+  pfa[E_SO_DESCENDING] = &pred_descending;
+
+  for (char i = 0; i < E_SO_END, i++) {
+    ia_bubble_sort(&iarray[0], ARRAY_SIZE, pfa[i]);
+    printf("\n");
+    ia_print(&iarray[0], ARRAY_SIZE);
+  }
+
   return 0;
+}
+
+bool pred_ascending(unsigned int a, unsigned int b) {
+  return a >= b;
+}
+
+bool pred_descending(unsigned int a, unsigned int b) {
+  return a < b;
 }
