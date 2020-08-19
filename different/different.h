640775fd9fd86b04f46082dbb524df739a010e18
Ruslan <rv.volovik88@gmail.com>
2020-08-19 14:14:38 +0300

    declaration of some functions

---
+++ different/different.h
@@ -0,0 +1,14 @@
+#ifndef DIFFERENT_H
+#define DIFFERENT_H
+
+#define STR_COPY
+
+#ifdef STR_COPY
+void strcopy_a(char *, char *);
+void strcopy_b(char *, char *);
+void strcopy_c(char *, char *);
+
+#elif defined(TEST_1)
+#endif
+
+#endif
