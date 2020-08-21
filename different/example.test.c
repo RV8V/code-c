e65c63bed10a4976e6c4f2035b6efd38fa31e55d
Ruslan <rv.volovik88@gmail.com>
2020-08-21 15:35:34 +0300

    realisation of its prototype from .h file

--- different/example.test.c
+++ different/example.test.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "example.test.h"
 
-#define LIM 10
-#define SIZE 20
-
-int main(int argc, char const *argv[]) {
+void example_test() {
   char c = 0, i = 0, str[SIZE];
   int *string = calloc(LIM - 1, sizeof(int));
   for (short int i = 0; i < LIM - 1 && (c = getchar()) != EOF && c != '\n'; ++i, string++)
@@ -12,6 +8,4 @@ int main(int argc, char const *argv[]) {
   for (short int i = 0; i < LIM - 1; i++, printf("%d\n", *string++));
   do; while(printf("%d,\n", *string++), i-- > 0);
   free(string);
-
-  return 0;
 }
