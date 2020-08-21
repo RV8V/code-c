07b97e94e7845d4d27b8bcdb946e974833f25ca8
Ruslan <rv.volovik88@gmail.com>
2020-08-21 15:38:22 +0300

    entry point into procedure for testing

--- different/main.c
+++ different/main.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include "main.h"
+#include "example.test.h"
+#include "limits.h"
 #include "different.h"
 
 int main(int argc, char const *argv[]) {
@@ -6,5 +8,10 @@ int main(int argc, char const *argv[]) {
   strcopy_c(s1, s2);
   puts(s1);
 
+  short int i = 0;
+  do {
+    print_size_of_types();
+    example_test();
+  } while(i == 0);
   return 0;
 }
