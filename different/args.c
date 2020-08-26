ffe6971ea39ad301bd1421e5e997f13e574a0020
Ruslan <rv.volovik88@gmail.com>
2020-08-26 21:24:11 +0300

    initial commit for args parametes in main function

--- different/args.c
+++ different/args.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[]) {
   printf("args count: %d\n", argc);
   printf("%p\n%p\n", argv, *argv);
-  for (int i = 0; i < argc; i++) {
-    printf("%d: %s\n", i, argv[i]);
+
+  if (argc < 3 || strlen(argv[1]) > 2 ||\
+     (argv[1][1] != 'p' && argv[1][1] != 'a' && argv[1][1] != 'c') ||\
+      atoi(argv[2]) == 0) {
+    printf("-p -> search for position number\n-c -> search for course number\n-a -> search for age");
+    return 0;
   }
+
+  for (int i = 0; i < argc; i++)
+    printf("%d: %s\n", i, argv[i]);
 
   return 0;
 }
