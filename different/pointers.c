cb75d230ea0826d6e1122d2e1dc2edeed0efa4ab
Ruslan <rv.volovik88@gmail.com>
2020-08-26 14:24:35 +0300

    pointers on varibles, pointer on pointer

--- different/pointers.c
+++ different/pointers.c
@@ -6,6 +6,7 @@ struct Test {
   unsigned char name[10];
 };
 
+void add_test(struct Test *, unsigned char *, char *, unsigned char *);
 void print_struct_data(void *);
 
 int main(int argc, char const *argv[]) {
@@ -13,11 +14,11 @@ int main(int argc, char const *argv[]) {
   printf("value a is: \t\t0x%08X\n", a);
 
   unsigned int *p_a = &a;
-  printf("value p_a is: \t\t0x%08d\n", (unsigned int)p_a);
+  //printf("value p_a is: \t\t0x%08d\n", (unsigned int)p_a);
   printf("value *p_a is: \t\t0x%08d\n", *p_a);
   *p_a = 0xEF222222;
   unsigned char uch[10] = { 0x33, 0x64, 0x65, 0x88, 0x32 };
-  printf("value uch is: \t\t0x%08d\n", (unsigned int)uch);
+  //printf("value uch is: \t\t0x%08d\n", (unsigned int)uch);
 
   unsigned int **p_p_a;
   p_a = &a;
@@ -35,7 +36,17 @@ int main(int argc, char const *argv[]) {
   strcpy(test.name, "test");
   print_struct_data((void *)&test);
 
+  unsigned char counter = 0, age = 20;
+  add_test(&test, &counter, "test for name", &age);
+  printf("counter value: %d\n", counter);
+
   return 0;
+}
+
+void add_test(struct Test *test, unsigned char *test_counter, char *ch, unsigned char *age) {
+  strcpy(test->name, ch);
+  test->age = *age;
+  (*test_counter)++;
 }
 
 void print_struct_data(void *p_str) {
