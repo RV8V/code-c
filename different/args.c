b849d02a8126cadd3f01851197619af298f70368
Ruslan <rv.volovik88@gmail.com>
2020-08-26 21:00:11 +0300

    initial commit for args parametes in main function

--- different/args.c
+++ different/args.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char const **argv) {
+int main(int argc, char const *argv[]) {
   printf("args count: %d\n", argc);
   printf("%p\n%p\n", argv, *argv);
-  for (int i = 0; i < argc; i++)
-    printf("%d: %s\n", i, *argv + i);
+  for (int i = 0; i < argc; i++) {
+    printf("%d: %s\n", i, argv[i]);
+  }
 
   return 0;
 }
