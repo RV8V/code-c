131e780dddf2c97a26ea4f50b4d93ceefb7b55c1
Ruslan <rv.volovik88@gmail.com>
2020-08-27 17:35:52 +0300

    test examples of pointer to pointer in c

---
+++ pointer_to_function/double_pointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char const **argv) {
+  int num = 123, *pr2, **pr1;
+  pr2 = &num; pr1 = &pr2;
+
+  printf("Value of num is: \t%d\n", num);
+  printf("Value of *pr2 is: \t%d\n", *pr2);
+  printf("Value of **pr1 is: \t%d\n\n", **pr1);
+
+  printf("address of num is: \t%p\n", &num);
+  printf("address of *pr2 is: \t%p\n", pr2);
+  printf("address of **pr1 is: \t%p\n\n", *pr1);
+
+  printf("value of pointer pr2: \t\t%p\n", pr2);
+  printf("value of pointer pr2 using pr1: \t%p\n\n", *pr1);
+
+  printf("address of pointer pr2: \t\t%p\n", &pr2);
+  printf("address of pointer pr2 using pr1: \t%p\n\n", pr1);
+
+  printf("Value of Pointer pr1 is: %p\n", pr1);
+  printf("Address of Pointer pr1 is: %p\n\n", &pr1);
+
+  if (num == *pr2 == **pr1) printf("%s\n", "ok");
+  if (&num == pr2 == *pr1) printf("%s\n", "ok");
+  if (&pr2 == pr1) printf("%s\n", "ok");
+}
