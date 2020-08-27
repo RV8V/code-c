2b394a1760a9ee885ef4a4bdf19cf92330495663
Ruslan <rv.volovik88@gmail.com>
2020-08-27 18:38:09 +0300

    initial commit for testing examples in c language

--- different/stuff.c
+++ different/stuff.c
@@ -3,4 +3,10 @@
 int main(argc, argv)
   int argc;
   char *argv[];
-{}
+{
+  char symbol;
+  char* word_of_symbols;
+  char** sentence_of_worlds;
+  char*** paragraph_of_sentences;
+  char**** and_so_on;
+}
